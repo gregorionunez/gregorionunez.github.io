991341ebb4c46a535d9a3b3696a8841b017fdb4c
gregorionunez <141684898+gregorionunez@users.noreply.github.com>
2023-09-27 10:39:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const corazon = document.getElementById("corazon");
+const barraLateral = document.querySelector(".barra-lateral"); //Obtenemos la referencia al primer elemento que coincida con ".barra-lateral"
+const spans = document.querySelectorAll("span"); //Obtenemos la referencia de todos los elementos que coincida con "span"
+const palanca = document.querySelector(".switch");
+const circulo = document.querySelector(".circulo");
+const menu = document.querySelector(".menu");
+const main = document.querySelector("main");
+
+menu.addEventListener("click",()=>{
+    barraLateral.classList.toggle("max-barra-lateral");
+    if(barraLateral.classList.contains("max-barra-lateral")){
+        menu.children[0].style.display = "none";
+        menu.children[1].style.display = "block";
+    }
+    else{
+        menu.children[0].style.display = "block";
+        menu.children[1].style.display = "none";  
+    }
+})
+
+palanca.addEventListener("click",()=>{
+    let body = document.body;
+    body.classList.toggle("modo-oscuro-activado");
+    circulo.classList.toggle("prendido");
+})
+
+
+corazon.addEventListener("click",()=>{
+    barraLateral.classList.toggle("mini-barra-lateral"); //Agrega clase si no la tiene
+    main.classList.toggle("min-main");
+    spans.forEach((span) => { //ForEach para recorrer todos los span
+        span.classList.toggle("oculto"); //Agrega clase si no la tiene
+    });
+})
