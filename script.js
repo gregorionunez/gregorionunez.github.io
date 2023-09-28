38af5b116993135210de30b671d01e6d590d5b0d
gregorionunez <141684898+gregorionunez@users.noreply.github.com>
2023-09-28 19:17:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const corazon = document.getElementById("barra-lateral-activar");
+const activacionBarra = document.getElementById("barra-lateral-activar");
 const barraLateral = document.querySelector(".barra-lateral"); //Obtenemos la referencia al primer elemento que coincida con ".barra-lateral"
 const spans = document.querySelectorAll("span"); //Obtenemos la referencia de todos los elementos que coincida con "span"
 const palanca = document.querySelector(".switch");
 const circulo = document.querySelector(".circulo");
 const menu = document.querySelector(".menu");
 const main = document.querySelector("main");
+const botonIngresar = document.querySelector(".boton-ingresar");
+const formLogin = document.querySelector(".form-login");
+const formRegister = document.querySelector(".form-register");
+const cerrarFormBtn = document.getElementById("cerrarForm");
+const fondoLogin = document.querySelector(".fondo-login");
 
 menu.addEventListener("click",()=>{
     barraLateral.classList.toggle("max-barra-lateral");
@@ -25,7 +30,7 @@ palanca.addEventListener("click",()=>{
 })
 
 
-corazon.addEventListener("click",(event)=>{
+activacionBarra.addEventListener("click",(event)=>{
     if (event.target.classList.contains("barra-lateral-activar")) {
     barraLateral.classList.toggle("mini-barra-lateral"); //Agrega clase si no la tiene
     main.classList.toggle("min-main");
@@ -33,3 +38,46 @@ corazon.addEventListener("click",(event)=>{
         span.classList.toggle("oculto"); //Agrega clase si no la tiene
     });
 }})
+
+let activado = false;
+
+botonIngresar.addEventListener("click",()=>{
+    if(activado){ 
+        formLogin.style.opacity = '0';
+        formLogin.style.left = "-99999px";
+        fondoLogin.style.opacity = '0';
+        fondoLogin.style.left = "-99999px";
+        activado = false;
+    }else{
+        formLogin.style.opacity = '1';
+        formLogin.style.left = "0";
+        fondoLogin.style.opacity = '1';
+        fondoLogin.style.left = "0";
+        activado = true;
+    }
+})
+
+cerrarFormBtn.addEventListener("click",()=>{
+    if(activado){ 
+        formLogin.style.opacity = '0';
+        formLogin.style.left = "-99999px";
+        fondoLogin.style.opacity = '0';
+        fondoLogin.style.left = "-99999px";
+        activado = false;
+    }
+})
+
+fondoLogin.addEventListener("click",()=>{
+    if(activado){ 
+        formLogin.style.opacity = '0';
+        formLogin.style.left = "-99999px";
+        fondoLogin.style.opacity = '0';
+        fondoLogin.style.left = "-99999px";
+
+        formRegister.style.opacity = '0';
+        formRegister.style.left = "-99999px";
+        fondoRegister.style.opacity = '0';
+        fondoRegister.style.left = "-99999px";
+        activado = false;
+    }
+})
